4ae38a4a2017985bb776ed6aa1834a80176be6dd
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-19 21:57:52 +0530

    Fix: Updated Navbar

--- components/Navbar/navbar.js
+++ components/Navbar/navbar.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 function navbar() {
   return (
-    <div>
-      <ul className="flex font-bold justify-center bg-slate-200">
-        <li className="p-2">Home</li>
-        <li className="p-2">About</li>
-        <li className="p-2">Blog</li>
-        <li className="p-2">Contact</li>
-      </ul>
+    <div className='flex justify-between bg-slate-200 px-20 py-2'>
+      <div>
+        <h1 className='text-2xl font-bold font-sans'>codeSlayers</h1>
+      </div>
+      <div>
+        <ul className='flex font-bold justify-center '>
+          <li className='p-2'>Home</li>
+          <li className='p-2'>About</li>
+          <li className='p-2'>Blog</li>
+          <li className='p-2'>Contact</li>
+        </ul>
+      </div>
+
     </div>
   );
 }
