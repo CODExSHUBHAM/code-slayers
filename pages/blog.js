3b616e170c829c97edf21521d4acadbb02ec333a
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-19 22:19:47 +0530

    Feat: Added about, blog, contact Pages

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/Navbar/navbar'
+
+function blog() {
+  return (
+    <>
+      <Navbar />
+      <div>blog</div>
+    </>
+  )
+}
+
+export default blog
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/Navbar/navbar'
+
+function contact() {
+  return (
+    <>
+      <Navbar />
+      <div>contact</div>
+    </>
+  )
+}
+
+export default contact
