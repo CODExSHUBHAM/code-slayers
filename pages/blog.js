c5b1091d092fbfbb65247cfbc12c935bad196ade
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-20 23:39:05 +0530

    Feat: populated blog cards using api

--- pages/blog.js
+++ pages/blog.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import Navbar from '@/components/Navbar/navbar'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import Navbar from '@/components/Navbar/navbar';
+import Link from 'next/link';
 
 
-function blog() {
+function Blog() {
+
+  const [blog, setblog] = useState([])
+
+  useEffect(() => {
+    console.log('hello')
+    fetch('http://localhost:3000/api/blogs')
+      .then((a) => { return a.json(); })
+      .then((data) => {
+        setblog(data)
+      })
+  }, []);
+
   return (
     <>
       <Navbar />
 
       <div className='p-8 flex justify-center'>
 
-        {/* Blog Card */}
-        <div className='bg-blue-100 w-80 p-3 rounded-xl m-4'>
-          <Link href={'/blogpost/javascript'}><h1 className='font-bold text-2xl mt-1'>Javascript</h1></Link>
-          <p className='mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, molestiae dolore! Iste, porro.</p>
-        </div>
+        {blog.map((blogitem) => {
+          return <div key={blogitem.slug} className='bg-blue-100 w-80 p-3 rounded-xl m-4'>
+            <Link href={'/blogpost/javascript'}><h1 className='font-bold text-2xl mt-1'>{blogitem.title}</h1></Link>
+            <p className='mt-2'>{blogitem.content.substr(0, 250)}</p>
+          </div>
+
+        })}
 
       </div>
     </>
   )
 }
 
-export default blog
+export default Blog
