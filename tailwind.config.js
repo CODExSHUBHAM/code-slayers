6fe0b228c9a44b1d61edf6118b42a62c4d800847
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-24 22:11:48 +0530

    Add: added custom font and colors to t config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      mono: ['Red Hat Mono', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      fontFamily: {
+        primary: ['Rubik', 'sans-serif'],
+      },
+      colors: {
+        primary: '#6230A3',
+        secondary: '#FBD587',
+        black: '#010101',
+        white: '#FFFFFF'
+      }
+    },
   },
   plugins: [],
 }
