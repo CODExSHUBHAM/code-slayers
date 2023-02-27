364c15bd1be720573572c218162bc7c9d3921a91
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-27 23:41:25 +0530

    feat: made footer and index page responsive

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -9,50 +9,52 @@ import arrow from '../../public/Icons/right-arrow.png';
 const Footer = () => {
   return (
     <>
-      <div className='flex items-center justify-between h-40 bg-blue-100 w-3/5 mx-auto rounded-2xl z-100 -mb-16 -mt-28 relative p-10 drop-shadow-2xl shadow-black'>
+      <div className='text-center md:text-left flex flex-col md:flex-row items-center justify-between md:h-40 bg-blue-100 w-4/5 md:w-3/5 mx-auto rounded-2xl z-100 -mb-16 -mt-28 relative p-4 md:p-10 drop-shadow-2xl shadow-black'>
         <div>
-          <h1 className='text-3xl text-primary font-primary font-semibold'>Want to write a blog?</h1>
-          <p className='font-medium pt-2'>Submit your blog here, and become a CODESLAYER</p>
+          <h1 className='text-2xl md:text-3xl text-primary font-primary font-semibold'>Want to write a blog?</h1>
+          <p className='font-medium text-sm md:text-base pt-2'>Submit your blog here, and become a CODESLAYER</p>
         </div>
         <Link href={'/submitblog'}>
-          <button className='flex items-center text-white bg-primary border-0 px-8 p-3 mx-2 focus:outline-none hover:bg-purple-900 rounded text-xl'>Get Started <Image src={arrow} alt="arrow" width={20} height={20} className='ml-2 pt-1' /></button>
+          <button className='flex items-center text-white bg-primary border-0 px-4 md:px-8 p-2 md:p-3 md:mx-2 my-2 md:my-0 focus:outline-none hover:bg-purple-900 rounded text-xl'>Get Started <Image src={arrow} alt="arrow" width={20} height={20} className='ml-2 pt-1' /></button>
         </Link>
       </div>
-      <div className='bg-dark h-96 flex flex-col justify-center items-center  px-20 pt-16 z-0'>
-        <div className='flex py-10'>
-          <div className='w-80 mb-32'>
+      <div className='bg-dark md:h-96 flex flex-col justify-center items-center pb-10 md:pb-0  px-20 pt-16 z-0'>
+        <div className='flex flex-col md:flex-row py-10'>
+          <div className='w-80 mb-10 md:mb-32'>
             <Link href={'/'}><Image src={whiteLogo} alt='white-logo' /></Link>
           </div>
-          <div className='px-20 pb-2'>
-            <h2 className='text-white font-medium text-lg'>Navigate</h2>
-            <ul className='text-secondary underline'>
-              <li className='p-1'><Link href='/'>Home</Link></li>
-              <li className='p-1'><Link href='/about'>About</Link></li>
-              <li className='p-1'><Link href='/blog'>Blog</Link></li>
-              <li className='p-1'><Link href='/contact'>Contact</Link></li>
-            </ul>
+          <div className='flex justify-between'>
+            <div className=' md:px-20 md:pb-2'>
+              <h2 className='text-white font-medium text-lg'>Navigate</h2>
+              <ul className='text-secondary underline'>
+                <li className='p-1'><Link href='/'>Home</Link></li>
+                <li className='p-1'><Link href='/about'>About</Link></li>
+                <li className='p-1'><Link href='/blog'>Blog</Link></li>
+                <li className='p-1'><Link href='/contact'>Contact</Link></li>
+              </ul>
+            </div>
+            <div className=' md:px-20 md:pb-2'>
+              <h2 className='text-white font-medium text-lg'>Famous Posts</h2>
+              <ul className='text-secondary underline'>
+                <li className='p-1'><Link href='blogpost/Javascript'>Javascript</Link></li>
+                <li className='p-1'><Link href='blogpost/sql'>Sql</Link></li>
+                <li className='p-1'><Link href='blogpost/react'>React Js</Link></li>
+                <li className='p-1'><Link href='blogpost/web-dev'>Web Development</Link></li>
+              </ul>
+            </div>
           </div>
-          <div className='px-20 pb-2'>
-            <h2 className='text-white font-medium text-lg'>Famous Posts</h2>
-            <ul className='text-secondary underline'>
-              <li className='p-1'><Link href='blogpost/Javascript'>Javascript</Link></li>
-              <li className='p-1'><Link href='blogpost/sql'>Sql</Link></li>
-              <li className='p-1'><Link href='blogpost/react'>React Js</Link></li>
-              <li className='p-1'><Link href='blogpost/web-dev'>Web Development</Link></li>
-            </ul>
-          </div>
-          <div className='px-16 pb-3'>
+          <div className='py-5 md:py-0 md:px-16 md:pb-3'>
             <h2 className='text-white font-medium text-lg'>Stay Up to Date</h2>
             <p className='w-48 text-secondary mt-4'>Subscribe our newsletter to get latest updates.</p>
-            <div className='flex mt-4'>
+            <div className='flex flex-col md:flex-row mt-4'>
               <input placeholder='enter email to subscribe' type="email" name="email" id="news-letter-email" className='w-full bg-gray-100 rounded border border-gray-300 focus:border-purple-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out' />
-              <button className='flex text-white bg-primary border-0 px-8 p-2 mx-2 focus:outline-none hover:bg-purple-900 rounded text-md'>Subscibe</button>
+              <button className='flex w-fit text-white bg-primary border-0 px-8 p-2 mt-4 md:mt-0 md:mx-2 focus:outline-none hover:bg-purple-900 rounded text-md'>Subscibe</button>
             </div>
           </div>
         </div>
-        <div className='py-2 text-white flex'>
+        <div className='md:py-2 text-white flex flex-col md:flex-row text-center'>
           <h1>Copyright ©2023 All rights reserved | This blog is developed by </h1>
-          <h2 className='text-secondary px-1'>CODExSHUBHAM</h2>
+          <h2 className='text-secondary md:px-1'>CODExSHUBHAM</h2>
         </div>
       </div>
     </>
--- components/Sections/aboutSection.js
+++ components/Sections/aboutSection.js
@@ -4,9 +4,9 @@ const AboutSection = () => {
   return (
     <>
       <section>
-        <div className='text-center p-10'>
+        <div className='text-center px-5 py-4 md:p-10'>
           <h1 className='text-3xl font-bold text-primary font-primary'>We are a community of coders</h1>
-          <p className='w-2/4 m-auto mt-6 font-primary font-medium leading-6 tracking-wide text-lg'>We are dedicated to providing the latest information on coding and technology trends and helping our readers stay up-to-date. We are also committed to fostering an inclusive, supportive environment where everyone can share their ideas and experiences.</p>
+          <p className='md:w-2/4 m-auto mt-6 font-primary font-medium leading-6 tracking-wide text-lg'>We are dedicated to providing the latest information on coding and technology trends and helping our readers stay up-to-date. We are also committed to fostering an inclusive, supportive environment where everyone can share their ideas and experiences.</p>
         </div>
       </section>
     </>
