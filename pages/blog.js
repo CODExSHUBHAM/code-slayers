ed4e1be827d715a4bc79e33a4a7343793a1c1e16
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-28 20:22:34 +0530

    feat: redesigned blogs page

--- pages/blog.js
+++ pages/blog.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 
@@ -9,13 +10,42 @@ const Blog = (props) => {
 
   return (
     <>
-      <section className='p-8 flex justify-center pb-40'>
-        {blogs.map((blogitem) => {
-          return <div key={blogitem.slug} className='bg-blue-100 w-80 p-3 rounded-xl m-4'>
-            <Link href={'/blogpost/' + blogitem.slug}><h1 className='font-bold text-2xl mt-1'>{blogitem.title}</h1></Link>
-            <p className='mt-2'>{blogitem.content.substr(0, 250)}...</p>
+      <section class="text-gray-600 body-font md:px-10 pb-10">
+        <div class="container px-5 py-24 mx-auto">
+          <h1 className='text-3xl md:text-4xl font-primary font-semibold w-fit m-auto pb-10 text-primary'>EXPLORE ALL BLOG'S</h1>
+          <div class="flex flex-wrap -m-4">
+            {blogs.map((blogitem) => {
+              return <div key={blogitem.slug} class="p-4 lg:w-1/3">
+                <div class="h-full bg-gray-900  px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative drop-shadow-xl">
+                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
+                  <h1 class="title-font sm:text-2xl text-xl font-medium text-white mb-3">{blogitem.title}</h1>
+                  <p class="leading-relaxed mb-3 text-secondary">{blogitem.content.substr(0, 200)}...</p>
+                  <a class="text-indigo-500 inline-flex items-center"><Link href={'/blogpost/' + blogitem.slug}>Learn More</Link>
+                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                  <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                    {/* <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                        <circle cx="12" cy="12" r="3"></circle>
+                      </svg>1.2K
+                    </span>
+                    <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                      </svg>6
+                    </span> */}
+                    <h2 class="tracking-widest text-sm title-font font-medium text-gray-400">By {blogitem.author}</h2>
+                  </div>
+                </div>
+              </div>
+            })}
+
           </div>
-        })}
+        </div>
       </section>
     </>
   )
