27e9c5c43db4b4c377984080dc926516663daff2
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-28 21:07:23 +0530

    fix: changed background color

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar/navbar';
 import Footer from '@/components/Footer/footer';
 
 export default function App({ Component, pageProps }) {
-  return <div className='bg-backdrop overflow-hidden'>
+  return <div className='bg-gray-300 overflow-hidden'>
     <Navbar />
     <Component {...pageProps} />
     <Footer />
