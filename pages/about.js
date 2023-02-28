1a40222a7ff684fa289133a2946a564201da1d03
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-02-28 22:17:45 +0530

    Feat: created temporary about and submit blog page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,11 @@ import React from 'react'
 const about = () => {
   return (
     <>
-      <section className='pb-40'>about</section>
+      <section className='pt-14 pb-40'>
+        <div className='w-4/5 m-auto text-center'>
+          <h1 className='text-primary font-primary font-bold tracking-wider text-4xl'>ABOUT US</h1>
+        </div>
+      </section>
     </>
   )
 }
--- pages/submitblog.js
+++ pages/submitblog.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Submitblog = () => {
   return (
-    <div>Submitblog</div>
+    <>
+      <section className='pt-14 pb-40'>
+        <div className='w-4/5 m-auto text-center'>
+          <h1 className='text-yellow-600 font-primary font-bold tracking-wider text-3xl underline leading-10'>We appreciate your effort but currently we are not accepting new blogs, please try after some time.</h1>
+        </div>
+      </section>
+    </>
   )
 }
 
