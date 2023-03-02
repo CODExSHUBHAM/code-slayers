19a24128b82b2d7eecb813da7a3bddc262d4c481
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-03-02 22:19:40 +0530

    Fix: minor bug fixes

--- pages/api/fetchblogs.js
+++ pages/api/fetchblogs.js
@@ -3,7 +3,7 @@ import connectDb from "@/middleware/connectDb";
 
 const handler = async (req, res) => {
   let blogs = await Blog.find()
-  res.status(200).json({ blogs })
+  res.status(200).json(blogs)
 }
 
 export default connectDb(handler);
