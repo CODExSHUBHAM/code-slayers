6137430e4cb4b1d9226289380a97c4966db23b67
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-03-03 10:11:43 +0530

    fix: ui fix in index js file

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ export default function Home() {
         <div className='py-5'>
           <AboutSection />
           <hr className='w-3/5 m-auto h-0.5 bg-dark' />
-          <div className="absolute px-4 -translate-x-1/2 bg-white left-1/2 dark:bg-gray-900">
-            <svg aria-hidden="true" className="w-5 h-5 text-gray-700 dark:text-gray-300" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" /></svg>
+          <div className="absolute px-4 -translate-x-1/2 bg-gray-900 left-1/2 dark:bg-gray-900">
+            <svg aria-hidden="true" className="w-5 h-5 text-white dark:text-white" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" /></svg>
           </div>
           <Cta />
         </div>
