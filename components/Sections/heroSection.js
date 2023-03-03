9a9029eaa970a486639aef533a3110a06725dd49
CODExSHUBHAM <shubham.nh10@gmail.com>
2023-03-03 19:34:16 +0530

    fix: updated image url with server url

--- components/Sections/heroSection.js
+++ components/Sections/heroSection.js
@@ -4,7 +4,7 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <>
-      <div className='bg-[url("../public/heroImg.jpg")] bg-fixed bg-contain md:bg-cover bg-center h-96 md:min-h-screen'>
+      <div className='bg-[url("https://codeslayers.vercel.app/heroImg.jpg")] bg-fixed bg-contain md:bg-cover bg-center h-96 md:min-h-screen'>
         <div className='bg-blue-900 bg-opacity-50 h-96 md:min-h-screen flex items-center justify-center backdrop-blur-sm'>
           <div className='inline-block text-center'>
             <p className='hidden md:block font-primary font-bold text-3xl lg:text-5xl w-full text-secondary border-r-4 border-purple-900 overflow-hidden whitespace-nowrap typing'>"Learn to Code - Discover Your Possibilities!"</p>
